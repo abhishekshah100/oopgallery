1240017da5bb8895cd5a9f3f81e0cb6a61c609bf
abhishekshah100 <53197002+abhishekshah100@users.noreply.github.com>
2019-09-09 12:06:50 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>PHPMailer</title>
+</head>
+<body>
+  <div class="form-box">
+  <h1>Simple Form</h1>
+  <form action="" method="post">  
+    <div class="form-group">
+      <label for="email">Email</label>
+      <input class="form-control" id="email" type="email" name="Email">
+    </div>
+    <input class="btn btn-primary" type="submit" value="Submit" />
+    </div>
+  </form>
+</div>
+
+</body>
+</html>
